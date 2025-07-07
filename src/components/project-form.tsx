6f69e4608ac709ec62ec27adaaf65f06f9d737f7
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-07 15:13:10 +0000

    correggi allineamento caselle prestazione importo lavori

--- src/components/project-form.tsx
+++ src/components/project-form.tsx
@@ -250,7 +250,7 @@ export function ProjectForm({ onAddProject, onUpdateProject, projectToEdit, onCl
             </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <FormField
             control={form.control}
             name="startDate"
