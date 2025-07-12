eb488e0392d02e1d7220cbfa3731fbc313ed8774
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-12 10:57:33 +0000

    TI FORNISCO IO SVG: <svg id="Livello_2" data-name="Livello 2" xmlns="htt

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,19 +1,26 @@
 export const GDAStudioLogo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
+    id="Livello_2"
+    data-name="Livello 2"
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 160 90"
+    viewBox="0 0 362 362"
     fill="currentColor"
     {...props}
   >
-    <text 
-      x="0" 
-      y="60" 
-      fontFamily="Inter, sans-serif" 
-      fontSize="60" 
-      fontWeight="bold"
+    <text
+      transform="translate(29 130.71)"
+      fontSize="138.17"
+      fontFamily="Lato-Black, Lato, sans-serif"
+      fontWeight="800"
+      letterSpacing="0.02em"
     >
-      GDA
+      G
+      <tspan x="101.14" y="0" letterSpacing="-0.01em">
+        D
+      </tspan>
+      <tspan x="202.28" y="0" letterSpacing="0.02em">
+        A
+      </tspan>
     </text>
-    <path d="M130 70 L130 90" stroke="currentColor" strokeWidth="10" />
   </svg>
 );
