9c1fda194b16fd85f5c7948b8849990b41564cb3
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-12 10:58:43 +0000

    UTILIZZIAMO SOLO IL TEMA CHIARO. UTILIZZA SVG CHE TI HO DATO ESATTAMENTE

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,26 +1,23 @@
 export const GDAStudioLogo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    id="Livello_2"
-    data-name="Livello 2"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 362 362"
-    fill="currentColor"
-    {...props}
-  >
-    <text
-      transform="translate(29 130.71)"
-      fontSize="138.17"
-      fontFamily="Lato-Black, Lato, sans-serif"
-      fontWeight="800"
-      letterSpacing="0.02em"
-    >
-      G
-      <tspan x="101.14" y="0" letterSpacing="-0.01em">
-        D
-      </tspan>
-      <tspan x="202.28" y="0" letterSpacing="0.02em">
-        A
-      </tspan>
-    </text>
-  </svg>
+    <svg id="Livello_2" data-name="Livello 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 362 362" {...props}>
+      <defs>
+        <style>
+          {`
+            .cls-1{fill:#36393e;}
+            .cls-2{font-size:84.78px;font-family:Lato-Regular, Lato;letter-spacing:-0.01em;}
+            .cls-2,.cls-3{fill:#d4d5d8;}
+            .cls-3{font-size:138.17px;font-family:Lato-Black, Lato;font-weight:800;letter-spacing:0.02em;}
+            .cls-4{letter-spacing:-0.01em;}
+            .cls-5{letter-spacing:0.02em;}
+          `}
+        </style>
+      </defs>
+      <rect className="cls-1" width="362" height="362"/>
+      <text className="cls-2" transform="translate(29 208.17)">STUDIO</text>
+      <text className="cls-3" transform="translate(29 130.71)">
+        G
+        <tspan className="cls-4" x="101.14" y="0">D</tspan>
+        <tspan className="cls-5" x="202.28" y="0">A</tspan>
+      </text>
+    </svg>
 );
